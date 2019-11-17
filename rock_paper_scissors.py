0b25d940b6e00af9844a492f7767323789f95b43
Davor Kandic <markojank727@gmail.com>
2019-11-17 23:18:26 +0100

    vs computer mode added, some refactoring also done

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,5 @@
 import sys
+from random import choice as rch
 
 def ending(num):
     if num == 1:
@@ -6,7 +7,28 @@ def ending(num):
     elif num == 2:
         print('        Player Two Wins!        ')
     elif num == 0:
-        print('        It\'s a Tie! !        ')
+        print('        It\'s a Tie! !          ')
+
+
+def compy():
+    return rch(valid_moves[:3])
+
+
+def close():
+    print()
+    print('Bye-bye!')
+    print()
+    sys.exit()
+
+
+def wrong():
+    print('    -----#######################-----     ')
+    print('  -----######__Wrong input!__#####------  ')
+    print('    -----#######################-----     ')
+
+
+def cheat_block():
+    print('****** NO CHEATING!!! ******\n\n' * 20)
         
 
 print('...rock...')
@@ -16,33 +38,40 @@ print()
 while True:
     valid_moves = ['rock', 'paper', 'scissors', 'r', 'p', 's']
     valid_dict = {'r': 'rock', 'p': 'paper', 's': 'scissors'}
+    while True:
+        print('Enter 1 if you want to play against human, or 2 to play vs computer:')
+        print('If you want to quit just enter "q"...')
+        opponent = input()
+        if opponent == '1' or opponent == '2' or opponent == 'q':
+            break
+        else:
+            wrong()
+    if opponent == 'q':
+        close()
     print('You can enter: "rock" or "r",\n"paper" or "p",\n"scissors" or "s"...')
     print('If you want to quit just enter "q"...')
     choice_1 = None
     while choice_1 not in valid_moves:
         choice_1 = input('Player One, enter your choice: ').lower()
         if choice_1 == 'q':
-            print()
-            print('Bye-bye!')
-            print()
-            sys.exit()
+            close()
         if choice_1 not in valid_moves:
-            print('  -----######__Wrong input!__#####------  ')
+            wrong()
     if choice_1 in valid_dict.keys():
         choice_1 = valid_dict[choice_1]  
-    print('****** NO CHEATING!!! ******\n\n' * 20)
+    cheat_block()
     choice_2 = None
-    while choice_2 not in valid_moves:
-        choice_2 = input('Player Two, enter your choice: ').lower()
-        if choice_2 == 'q':
-            print()
-            print('Bye-bye!')
-            print()
-            sys.exit()
-        if choice_2 not in valid_moves:
-            print('  -----######__Wrong input!__#####------  ')
-    if choice_2 in valid_dict.keys():
-        choice_2 = valid_dict[choice_2]
+    if opponent == '2':
+        choice_2 = compy()
+    else:
+        while choice_2 not in valid_moves:
+            choice_2 = input('Player Two, enter your choice: ').lower()
+            if choice_2 == 'q':
+                close()
+            if choice_2 not in valid_moves:
+                wrong()
+        if choice_2 in valid_dict.keys():
+            choice_2 = valid_dict[choice_2]
     print('          SHOOT!          ')
     if choice_1 == choice_2:
         ending(0)
